e3ba5d28d31aeb2ba99687a9d5a99171175a2239
Ewen <ewengue1432@gmail.com>
2025-10-26 21:53:33 -0400

    fast api testing

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,16 @@
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+
+app = fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/test")
+def read_root():
+    return {"Hello": "World"}
