911230a4d62cd38355a59d6062887b364e24832e
Ewen <ewengue1432@gmail.com>
2025-10-28 23:40:22 -0400

    pdf download and display frontend

---
+++ frontend/src/backend/server_posts/post.tsx
@@ -0,0 +1,58 @@
+import { API_URL } from "@/consts";
+
+export async function postTextContent(str: string) {
+  try {
+    const response = await fetch(`${API_URL}/resume`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ resume: str }),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to save resume");
+    }
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function getPDFDownload(str: string) {
+  try {
+    const response = await fetch(`${API_URL}/pdf-download`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ resume: str }),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to save resume");
+    }
+    const data = await response.blob();
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export async function getPDFDisplay(str: string) {
+  try {
+    const response = await fetch(`${API_URL}/pdf-display`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ resume: str }),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to save resume");
+    }
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+}
